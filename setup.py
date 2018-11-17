d503ea6f88dfaae3a0841dea08c1141ff00eef7a
Tim Woocker <tim.woocker@googlemail.com>
2018-11-17 19:59:50 +0100

    add some verification steps

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache-2.0",
         "Operating System :: OS Independent",
-    ],
+    ]
 )
