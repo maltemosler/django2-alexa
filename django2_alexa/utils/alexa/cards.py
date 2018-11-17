dd9070ef547ecf0f780c85254ffbaaeb13e2cc78
Tim Woocker <tim.woocker@googlemail.com>
2018-11-18 00:49:58 +0100

    simple fix

--- django2_alexa/utils/alexa/cards.py
+++ django2_alexa/utils/alexa/cards.py
@@ -3,7 +3,6 @@ from django.http import HttpRequest
 
 class SimpleCard:
     def __init__(self, request: HttpRequest):
-        super().__init__(request)
         self.type = "Simple"
         self.title = ""         # type: str
         self.content = ""       # type: str
@@ -11,7 +10,6 @@ class SimpleCard:
 
 class StandardCard:
     def __init__(self, request: HttpRequest):
-        super().__init__(request)
         self.type = "Standard"
         self.title = ""             # type: str
         self.text = ""              # type: str
@@ -22,13 +20,11 @@ class StandardCard:
 
 class LinkAccountCard:
     def __init__(self, request: HttpRequest):
-        super().__init__(request)
         self.type = "LinkAccount"
 
 
 class AskForPermissionsConsentCard:
     def __init__(self, request: HttpRequest):
-        super().__init__(request)
         self.type = "AskForPermissionsConsent"
         self.title = ""         # type: str
         self.content = ""       # type: str
