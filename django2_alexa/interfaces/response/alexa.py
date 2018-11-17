6958d9ad46a231130d3ae9a9a45f2dab14e031b9
Tim Woocker <tim.woocker@googlemail.com>
2018-11-18 00:45:35 +0100

    output speech

---
+++ django2_alexa/interfaces/response/alexa.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+from django2_alexa.interfaces.response.output_speech import OutputSpeech
+
+
+class Response:
+    def __init__(self, output_speech: OutputSpeech=None):
+        pass
