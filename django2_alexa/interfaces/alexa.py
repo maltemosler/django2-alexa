1fa81619fbdd09080eb086bbb366cf57c6aa3856
Tim Woocker <tim.woocker@googlemail.com>
2018-11-18 20:29:15 +0100

    func for wrapper

--- django2_alexa/interfaces/alexa.py
+++ django2_alexa/interfaces/alexa.py
@@ -8,14 +8,20 @@ from django2_alexa.utils.s3_verification import is_valid_request
 
 
 class Skill:
+    # Standard
     _launch = None
     _intents = {}
+
+    # AudioPlayer
+    _playback_started = None
 
     def __init__(self):
         self.view = csrf_exempt(self._view)
 
     def _view(self, request, *args, **kwargs):
         re = BaseRequest(request)
+
+        # Standard Requests
         if re.type == "LaunchRequest" and self._launch:
             return self._launch(request, *args, **kwargs)
         if re.type == "IntentRequest":
@@ -23,25 +29,32 @@ class Skill:
             if name in self._intents:
                 return self._intents[name](request, *args, **kwargs)
 
-    def launch(self, func):
-        self._launch = func
+        # AudioPlayer Requests
+        if re.type == "AudioPlayer.PlaybackStarted" and self._playback_started:
+            return self._playback_started(request, *args, **kwargs)
 
+    @staticmethod
+    def _wrapper(func, req):
         def wrapper(request, *args, **kwargs):
             if getattr(settings, "ALEXA_VERIFY_CONN", False) and not is_valid_request(request):
-                # TODO: Troubleshooting part in docs
                 return HttpResponseServerError("Amazon Server verification failed.")
-            request.launch_request = LaunchRequest(request)
+            request.alexa_request = req(request)
             return func(request, *args, **kwargs)
+        return wrapper
+
+    def launch(self, func):
+        self._launch = func
+        wrapper = self._wrapper(func, LaunchRequest)
         return wrapper
 
     def intent(self, name: str):
         def inner(func):
             self._intents[name] = func
-
-            def wrapper(request, *args, **kwargs):
-                if getattr(settings, "ALEXA_VERIFY_CONN", False) and not is_valid_request(request):
-                    return HttpResponseServerError("Amazon Server verification failed.")
-                request.intent_request = IntentRequest(request)
-                return func(request, *args, **kwargs)
+            wrapper = self._wrapper(func, IntentRequest)
             return wrapper
         return inner
+
+    # def playback_started(self, func):
+    #     self._playback_started = func
+    #     wrapper = self._wrapper(func, PlaybackStartedRequest)
+    #     return wrapper
