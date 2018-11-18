b8b939d684bec1fe92bf00234e7f9a9f302157a7
Malte <deemonrider@gmx.de>
2018-11-18 01:18:06 +0100

    updated serializer for cards

--- django2_alexa/utils/alexa/cards.py
+++ django2_alexa/utils/alexa/cards.py
@@ -7,10 +7,10 @@ class Card:
 
 
 class SimpleCard(Card):
-    def __init__(self):
+    def __init__(self, title: str, content: str):
         self.type = "Simple"
-        self.title = ""  # type: str
-        self.content = ""  # type: str
+        self.title = title
+        self.content = content
 
     def serialize(self):
         d = {
@@ -22,23 +22,26 @@ class SimpleCard(Card):
 
 
 class StandardCard(Card):
-    def __init__(self):
+    def __init__(self, title: str, text: str, small_image_url: str="", large_item_url: str=""):
         self.type = "Standard"
-        self.title = ""  # type: str
-        self.text = ""  # type: str
-        self.smallImageUrl = ""  # type: str
-        self.largeImageUrl = ""  # type: str
+        self.title = title
+        self.text = text
+        self.small_image_url = small_image_url
+        self.large_item_url = large_item_url
 
     def serialize(self):
         d = {
             'type': self.type,
             'title': self.title,
-            'text': self.text,
-            'image': {
-                'smallImageUrl': self.smallImageUrl,
-                'largeImageUrl': self.largeImageUrl
-            },
+            'text': self.text
         }
+        if self.small_image_url or self.large_item_url:
+            d['image'] = {}
+            if self.small_image_url:
+                d['image']['smallImageUrl'] = self.small_image_url
+            if self.large_item_url:
+                d['image']['large_item_url'] = self.large_item_url
+
         return d
 
 
@@ -54,11 +57,11 @@ class LinkAccountCard(Card):
 
 
 class AskForPermissionsConsentCard(Card):
-    def __init__(self):
+    def __init__(self, title: str, content: str, text: str):
         self.type = "AskForPermissionsConsent"
-        self.title = ""  # type: str
-        self.content = ""  # type: str
-        self.text = ""  # type: str
+        self.title = title
+        self.content = content
+        self.text = text
 
     def serialize(self):
         d = {
