0fbf871a0b2af0a120299956aac6906c3ff6de9a
Malte <deemonrider@gmx.de>
2018-11-18 17:30:11 +0100

    intent request

--- django2_alexa/interfaces/request/alexa.py
+++ django2_alexa/interfaces/request/alexa.py
@@ -9,21 +9,21 @@ from django2_alexa.utils.enums.locales import Locale
 class LaunchRequest(BaseRequest):
     def __init__(self, request: HttpRequest):
         super().__init__(request)
-        self.request_id = self.body["requestId"]    # type: str
-        self.timestamp = self.body["timestamp"]     # type: str
-        self.locale = Locale(self.body["locale"])   # type: Locale
+        self.request_id = self.body["requestId"]  # type: str
+        self.timestamp = self.body["timestamp"]  # type: str
+        self.locale = Locale(self.body["locale"])  # type: Locale
 
 
 class IntentRequest(BaseRequest):
     def __init__(self, request: HttpRequest):
         super().__init__(request)
-        self.request_id = self.body["requestId"]    # type: str
-        self.timestamp = self.body["timestamp"]     # type: str
-        self.locale = Locale(self.body["locale"])   # type: Locale
+        self.request_id = self.body["requestId"]  # type: str
+        self.timestamp = self.body["timestamp"]  # type: str
+        self.locale = Locale(self.body["locale"])  # type: Locale
         self.dialog_state = DialogState(self.body["dialogState"])
         intent = self.body["intent"]
         slots = {}
-        for s in intent["slots"]:
-            for x in s["SlotName"]:
-                slots[x.name] = Slot(x.name, x.value, x.confirmation_status)
-        self.intent = Intent(intent["name"], ConfirmationStatus(intent["confirmationStatus"]))
+        for name in intent["slots"]:
+            slots[name] = Slot(name, intent["slots"][name]["value"],
+                               ConfirmationStatus(intent["slots"][name]["confirmationStatus"]))
+        self.intent = Intent(intent["name"], ConfirmationStatus(intent["confirmationStatus"]), slots)
